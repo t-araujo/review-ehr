b6d44ddc24a837d1dcbcf3be4b87bb2e97ac9739
Tiago Araujo <taraujo@moviik.com>
2021-03-19 09:17:42 +0000

    [backend] Add Mongoose connection to app

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,23 @@ const body = require('koa-body')
 const cors = require('@koa/cors')
 const helmet = require('koa-helmet')
 const Koa = require('koa')
+const Mongoose = require('mongoose')
+const { get } = require('lodash')
 const { routeLog } = require('src/middlewares')
+
+const mongooseOptions = {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}
+
+const mongo_address = get(process.env, 'MONGO_ADDRESS', 'localhost')
+const mongo_port = get(process.env, 'MONGO_PORT', '27017')
+const mongo_db = get(process.env, 'MONGO_DB', 'gyant')
+const mongo_user = get(process.env, 'MONGO_INITDB_ROOT_USERNAME')
+const mongo_pass = get(process.env, 'MONGO_INITDB_ROOT_PASSWORD')
+
+Mongoose.connect(`mongodb://${mongo_user}:${mongo_pass}@${mongo_address}:${mongo_port}/${mongo_db}?authSource=admin`, mongooseOptions)
 
 const app = new Koa()
 app.use(helmet())
