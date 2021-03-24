7bf113bf747814e8698f22fdb5e029d7d6fa2238
Tiago Araujo <taraujo@moviik.com>
2021-03-24 00:56:26 +0000

    [frontend] Add types for actions

--- frontend/src/actions/types.js
+++ frontend/src/actions/types.js
@@ -1,4 +1,18 @@
-export const API_URL = "http://localhost:4000/api"
+export const API_URL = process.env.REACT_APP_API_ENDPOINT || "http://localhost:4000/api"
+
+export const LOGIN_REQUEST = "LOGIN_REQUEST"
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS"
 export const LOGIN_FAIL = "LOGIN_FAIL"
 export const LOGOUT = "LOGOUT"
+
+export const SET_MESSAGE = "SET_MESSAGE";
+export const CLEAR_MESSAGE = "CLEAR_MESSAGE";
+
+export const CASES_SUCCESS = "CASES_SUCCESS"
+export const CASES_FAIL = "CASES_FAIL"
+
+export const CONDITIONS_SUCCESS = "CONDITIONS_SUCCESS"
+export const CONDITIONS_FAIL = "CONDITIONS_FAIL"
+
+export const DECISIONS_SUCCESS = "DECISIONS_SUCCESS"
+export const DECISIONS_FAIL = "DECISIONS_FAIL"
