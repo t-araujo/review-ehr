190b76938fff3fac789a1d69e1c6b19633b34b9d
Tiago Araujo <taraujo@moviik.com>
2021-03-24 00:52:30 +0000

    [frontend] Add reducers to index

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,8 +1,16 @@
 import { combineReducers } from "redux"
 import auth from "./auth"
+import message from "./message"
+import cases from "./cases"
+import conditions from "./conditions"
+import decisions from "./decisions"
 
 const rootReducer = combineReducers({
   auth,
+  message,
+  cases,
+  conditions,
+  decisions,
 })
 
 export default rootReducer
