a4ee1ac369e94cb5a49654174a1fb13204a9e336
Tiago Araujo <taraujo@moviik.com>
2021-03-24 01:12:20 +0000

    [frontend] Add Login page and home page to app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
+import { history } from './helpers/history';
+import LoginPage from './containers/login/login'
+import HomePage from './containers/home/home'
 
 function App() {
   return (
-    <div>
-    </div>
+  <Router history={history}>
+    <Switch>
+        <Route path="/login" component={LoginPage} />
+        <Route path="/" component={HomePage} />
+        <Redirect from="*" to="/" />
+    </Switch>
+  </Router>
   );
 }
 
