03141543269e2d0b4f956805d62fa1c1df8dcf91
Tiago Araujo <taraujo@moviik.com>
2021-03-24 02:34:02 +0000

    [backend] Add possibility to pass query parameter to route cases

--- backend/src/interfaces/cases.js
+++ backend/src/interfaces/cases.js
@@ -2,6 +2,12 @@ const { CasesController } = require('src/controllers')
 
 module.exports = (router) => {
   router.get('/cases', async (ctx) => {
+    if (ctx.request.query.withoutdecisions) {
+      ctx.response.body = await CasesController.getCasesWithoutDecisions()
+
+      return
+    }
+
     ctx.response.body = await CasesController.getCases()
   })
 }
--- backend/test/interfaces/cases.js
+++ backend/test/interfaces/cases.js
@@ -8,5 +8,13 @@ describe('Cases interface', () => {
       response.should.have.status(200)
       response.body.should.be.a('array')
     })
+
+    it('Should get all cases', async function () {
+      const response = await chai.request(app)
+        .get('/api/cases?withoutdecisions=true')
+        .set({ Authorization: `Bearer ${this.authenticatedUser().token}` })
+
+      response.should.have.status(200)
+    })
   })
 })
