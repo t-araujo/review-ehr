ee422439de6ec74e8bbdb12d0e76cb45f5a03d46
Tiago Araujo <taraujo@moviik.com>
2021-03-24 02:32:50 +0000

    [backend] Add method to get cases without decisions

--- backend/src/controllers/cases.js
+++ backend/src/controllers/cases.js
@@ -1,8 +1,15 @@
-const { Cases } = require('src/models')
+const { Cases, CasesDecisions } = require('src/models')
 
 class CasesController {
   static async getCases () {
     return Cases.find({})
+  }
+
+  static async getCasesWithoutDecisions () {
+    const cases_decisions = await CasesDecisions.find({})
+    const casesids = cases_decisions.map((cas) => cas.case_id)
+    const cases = await Cases.find().where('_id').nin(casesids).exec()
+    return cases
   }
 }
 
